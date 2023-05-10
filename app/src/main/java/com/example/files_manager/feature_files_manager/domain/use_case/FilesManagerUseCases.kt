865c42bbf486ec2f522e965cae61219d0c36589a
DimaEff <fominienkovd@yandex.ru>
2023-05-10 20:30:45 +0300

    added the files manager use cases

---
+++ app/src/main/java/com/example/files_manager/feature_files_manager/domain/use_case/FilesManagerUseCases.kt
@@ -0,0 +1,11 @@
+package com.example.files_manager.feature_files_manager.domain.use_case
+
+import com.example.compose_test.feature_files_manager.domain.use_case.GetFilesByPath
+import com.example.compose_test.feature_files_manager.domain.use_case.OrderFiles
+import com.example.compose_test.feature_files_manager.domain.use_case.RunIndexingChangedFiles
+
+data class FilesManagerUseCases(
+    val  getFilesByPath: GetFilesByPath,
+    val orderFiles: OrderFiles,
+    val runIndexingChangedFiles: RunIndexingChangedFiles,
+)
